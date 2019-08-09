6466f4b885a3c0190ec6368a3be84c473c947f31
andrelohmann <lohmann.andre@gmail.com>
2019-08-09 09:24:17 +0200

    Set up React Router

--- application/src/index.js
+++ application/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'
+//import logger from 'redux-logger'
 import { Provider } from 'react-redux';
 import './stylesheets/index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, applyMiddleware(thunk));//, logger));
 
 ReactDOM.render(
   <Provider store={store}>
